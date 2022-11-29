73a0a6513a4c7e1699cac7395970de007c4e43d1
sooreoluaw <okeyodesamuel@gmail.com>
2022-11-29 08:30:37 +0100

    new commit

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+// var mNumber = document.getElementById("matricNumber").value;
+// alert(mNumber);
+//bu19cit1031
+
+function myFunction() {
+    // var mNumber = document.getElementById("matricNumber").value;
+    // alert(mNumber);
+    let text = document.getElementById("matricNumber").value;
+    let result1 = text.includes("CIT", 4);
+    let result2 = text.includes("CYB", 4);
+    let result3 = text.includes("SEN", 4);
+    let result4 = text.includes("CSC", 4);
+    if(result1 || result2 || result3 || result4 == true){
+    window.open("ballot.html","_blank");
+    return false;
+    }else{
+        alert("You are not a computing student");
+    }
+  }
