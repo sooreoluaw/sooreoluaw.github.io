14d5d90251699132e374dc86398affdaf84ab2ff
sooreoluaw <okeyodesamuel@gmail.com>
2022-11-29 08:45:35 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,8 @@ function myFunction() {
     let result2 = text.includes("CYB", 4);
     let result3 = text.includes("SEN", 4);
     let result4 = text.includes("CSC", 4);
+    let result5 = text.includes("CBS", 4);
+    let result6 = text.includes("IFT", 4);
     if(result1 || result2 || result3 || result4 == true){
     window.open("ballot.html","_blank");
     return false;
